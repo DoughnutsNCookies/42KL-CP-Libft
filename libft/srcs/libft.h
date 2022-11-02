b19d94f4b16feb7772bb6c5ea19582f3139aae95
Chuah Tse Yung <chuahtseyung2002@gmail.com>
2022-11-02 14:36:45 +0800

    Added ft_split_charset.c

--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:03:32 by schuah            #+#    #+#             */
-/*   Updated: 2022/10/31 16:41:58 by schuah           ###   ########.fr       */
+/*   Updated: 2022/11/02 14:30:34 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,8 +57,10 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strjoin_free(char *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-void	ft_freesplit(char **split);
+char	**ft_split_charset(char const *s, char *str);
 int		ft_getwc(char const *s, char c);
+int		ft_getwc_charset(char const *s, char *str);
+void	ft_freesplit(char **split);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
